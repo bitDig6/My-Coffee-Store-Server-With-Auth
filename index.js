01bc27d3f9ca0f4bc0681b3227fc76efac01c8aa
bitDig6 <ummenabilajalal@gmail.com>
2025-04-30 18:15:58 +0600

    hosted on vercel

--- index.js
+++ index.js
@@ -31,40 +31,40 @@ async function run() {
 
     //create database and db collection for your coffee store
     const database = client.db("CoffeeStoreDB");
-    const coffeeCollection  = database.collection("coffee");
+    const coffeeCollection = database.collection("coffee");
+    const usersCollection = client.db("CoffeeStoreDB").collection("coffeeStoreUsers");
 
-    app.get('/coffee', async(req, res) => {
+    app.get('/coffee', async (req, res) => {
       const cursor = coffeeCollection.find();
-      const result =  await cursor.toArray();
+      const result = await cursor.toArray();
       res.send(result);
     })
 
-    app.get('/coffee/:id', async(req, res) => {
+    app.get('/coffee/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await coffeeCollection.findOne(query);
       res.send(result);
     })
 
-    app.post('/coffee', async(req, res) => {
-        const newCoffee = req.body;
-        console.log(newCoffee);
-        const result = await coffeeCollection.insertOne(newCoffee);
-        res.send(result);
+    app.post('/coffee', async (req, res) => {
+      const newCoffee = req.body;
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
     })
 
-    app.delete('/coffee/:id', async(req, res) => {
+    app.delete('/coffee/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await coffeeCollection.deleteOne();
       res.send(result);
     })
 
-    app.put('/coffee/:id', async(req, res) => {
+    app.put('/coffee/:id', async (req, res) => {
       const id = req.params.id;
       const newCoffee = req.body;
-      const filter = { _id: new ObjectId(id)};
-      const options = {upsert: true};
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
       const query = {
         $set: {
           name: newCoffee.name,
@@ -76,8 +76,43 @@ async function run() {
           details: newCoffee.details
         }
       }
-
       const result = await coffeeCollection.updateOne(filter, query, options);
+      res.send(result);
+    })
+
+    //users related apis
+
+    app.get('/users', async (req, res) => {
+      const cursor = usersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/users', async (req, res) => {
+      const newUser = req.body;
+      console.log('creating new user', newUser);
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    })
+
+    app.patch('/users/', async (req, res) => {
+      const email = req.body.email;
+      const filter = { email };
+      const updatedDoc = {
+        $set: {
+          lastSignInTime: req.body?.lastSignInTime
+          
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = usersCollection.deleteOne(query);
+      console.log(result);
       res.send(result);
     })
 
@@ -89,9 +124,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Coffee Store Server is Running');
+  res.send('Coffee Store Server is Running');
 })
 
 app.listen(port, () => {
-    console.log(`Coffee Store Server is running at Port: ${port}`);
+  console.log(`Coffee Store Server is running at Port: ${port}`);
 })
